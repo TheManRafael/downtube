387377a93d8a14401d5eeea8b10122c74d22d8d3
TheManRafael <107126420+TheManRafael@users.noreply.github.com>
2023-09-23 15:41:38 +0200

    Update downTube.py
    Código melhorado e organizado

--- downTube.py
+++ downTube.py
@@ -1,10 +1,11 @@
 import os
-from pytube import YouTube
-from moviepy.editor import AudioFileClip
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
+from pytube import YouTube
+from moviepy.editor import AudioFileClip
 
+# Função para baixar e processar o vídeo
 def baixar_video():
     url = url_entry.get()
     formato = formato_combo.get()
@@ -12,66 +13,68 @@ def baixar_video():
     yt = YouTube(url)
 
     if formato == 'mp3':
-        stream = yt.streams.filter(only_audio=True).first()
-        progress_var.set("Baixando...")
-        root.update()
-        file_path = stream.download(output_path="videos")
-        progress_var.set("Download concluído. Convertendo para mp3...")
-        root.update()
-
-        clip = AudioFileClip(file_path)
-        clip.write_audiofile(file_path.replace(".mp4", ".mp3"))
-
-        os.remove(file_path)
-        progress_var.set("Conversão concluída.")
-        root.update()
+        download_e_converter_para_mp3(yt)
     else:
-        if formato == 'mkv':
-            stream = yt.streams.filter(file_extension='webm').get_highest_resolution()
-        elif formato == 'mp4':
-            stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+        baixar_video(yt, formato)
 
-        progress_var.set("Baixando...")
-        root.update()
-        stream.download(output_path="videos")
-        progress_var.set("Download concluído.")
-        root.update()
+# Função para baixar e converter o vídeo para MP3
+def download_e_converter_para_mp3(yt):
+    stream = yt.streams.filter(only_audio=True).first()
+    progress_var.set("Baixando...")
+    root.update()
+    file_path = stream.download(output_path="videos")
+    progress_var.set("Download concluído. Convertendo para mp3...")
+    root.update()
 
+    clip = AudioFileClip(file_path)
+    clip.write_audiofile(file_path.replace(".mp4", ".mp3"))
+
+    os.remove(file_path)
+    progress_var.set("Conversão concluída.")
+    root.update()
+
+# Função para baixar o vídeo no formato especificado
+def baixar_video(yt, formato):
+    if formato == 'mkv':
+        stream = yt.streams.filter(file_extension='webm').get_highest_resolution()
+    elif formato == 'mp4':
+        stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+
+    progress_var.set("Baixando...")
+    root.update()
+    stream.download(output_path="videos")
+    progress_var.set("Download concluído.")
+    root.update()
+
+# Criar a janela principal
 root = ThemedTk(theme="arc")
 root.geometry('600x400')
-root.title("DownTube - Baixador de Vídeos do YouTube")  # Título da janela
+root.title("DownTube - Baixador de Vídeos do YouTube")  
 
-logo = tk.PhotoImage(file="logo.png")  # Substitua 'logo.png' pelo nome do seu arquivo de imagem
+# Elementos da interface gráfica (UI)
+logo = tk.PhotoImage(file="logo.png")
 logo_label = ttk.Label(root, image=logo)
-logo_label.pack(pady=10)
-
 dev_label = ttk.Label(root, text="Desenvolvido por: Uélinton Morelli")
-dev_label.pack()
-dev_label.pack(pady=10)
-
 title = ttk.Label(root, text="DownTube - Baixador de Vídeos do YouTube", font=("Helvetica", 16))
-title.pack(pady=10)
-
 url_label = ttk.Label(root, text="URL do YouTube:")
-url_label.pack()
-
 url_entry = ttk.Entry(root, width=50)
-url_entry.pack(pady=5)
-
 formato_label = ttk.Label(root, text="Formato:")
-formato_label.pack()
-
-# Combobox para selecionar o formato
 formato_combo = ttk.Combobox(root, values=["mp3", "mp4", "mkv"], state="readonly")
-formato_combo.pack(pady=5)
-
 download_button = ttk.Button(root, text="Baixar", command=baixar_video)
-download_button.pack(pady=10)
-
 progress_var = tk.StringVar()
 progress_var.set("Esperando...")
-
 progress_label = ttk.Label(root, textvariable=progress_var)
+
+# Organizar elementos da interface gráfica
+logo_label.pack(pady=10)
+dev_label.pack(pady=10)
+title.pack(pady=10)
+url_label.pack()
+url_entry.pack(pady=5)
+formato_label.pack()
+formato_combo.pack(pady=5)
+download_button.pack(pady=10)
 progress_label.pack()
 
+# Rodar o loop principal
 root.mainloop()
